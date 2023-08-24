1e626031615c012a8d96f8539daa0d13dd9ffadd
Silvaoscar <oscarsilva03@yahoo.com>
2023-08-24 14:04:54 +0200

    new controller register

--- controller/controllerRegister.js
+++ controller/controllerRegister.js
@@ -1,1 +1,14 @@
-//for register
+import {User} from '../model/modelSchema.js';
+
+
+export const register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const userFound = await User.findOne({ email });
+
+        if (userFound)
+            return res.status(400).json({ message: ["The email is already in use"] });
+    } catch (err) {
+        console.error(err);
+    }
+};
--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv/config';
 import connectMongoose from './connectDB.js';
+import router from './router/routerProfile.js';
 
 const app = express();
 const port = process.env.PORT
+
+
+app.use(express.json());
+
+app.use('/api', router);
+
+
 
 connectMongoose();
 app.listen(port);
