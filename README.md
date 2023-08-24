5ec6801df8b1323e9774933fdb1b15cc0eba32f1
Dana <dana@hlavacova.de>
2023-08-24 14:01:04 +0200

    bcrypt

---
+++ README.md
@@ -0,0 +1,13 @@
+1. **environment setup**
+
+- packetsjson.,
+- packetens:  "cors": "^2.8.5",
+  "dotenv": "^16.3.1",
+  "express": "^4.18.2",
+  "mongodb": "^5.8.0",
+  "mongoose": "^7.4.4",
+  "morgan": "^1.10.0",
+  "bcrypt": "^5.1.0",
+  "nodemon": "^3.0.1"
+- file: .env, .gitignore, contoller, model, routes, server.js,
+
--- controller/controllerLogin.js
+++ controller/controllerLogin.js
@@ -1,1 +1,21 @@
-//für login
+import {User} from "../model/modelSchema.js";
+/**
+ * Handles the logout post request.
+ * @param {import('express').Request} req - The request object.
+ * @param {import('express').Response} res - The response object.
+ */
+
+//login User
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body;
+    try {
+        const user=await User.findOne({email});
+        if(!user) {
+            return res.status(404).send({success: false, error: 'User/Password Combination not found'});
+        }
+        const isCorrectPassword=await
+    }
+    catch (e) {
+        console.log(e);
+    }
+}
