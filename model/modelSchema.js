f976d065504db5c2279237c038aa2d85b76141e4
Dana <dana@hlavacova.de>
2023-08-28 09:57:00 +0200

    login is in ornung - jwt+bcrypt

--- model/modelSchema.js
+++ model/modelSchema.js
@@ -1,11 +1,25 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String,
     role: String,
-
 });
 
-export const User = mongoose.model('User', UserSchema);
+UserSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    console.log({salt});
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    console.log({hashedPassword});
+    next();
+});
+
+UserSchema.methods.isPasswordValid = async function (password) {
+    const compare = await bcrypt.compare(password, this.password);
+    return compare;
+};
+
+export const User = mongoose.model("User", UserSchema);
