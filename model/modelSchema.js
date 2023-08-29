9c7ae19ec5aad0c07bb3bf92048fb03a382ddd25
Dana <dana@hlavacova.de>
2023-08-29 11:50:28 +0200

    datenbank validation works

--- model/modelSchema.js
+++ model/modelSchema.js
@@ -2,24 +2,24 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
-    name: String,
-    email: String,
-    password: String,
-    role: String,
+  name: { type: String, required: true, minlength: 3, maxlength: 50 },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, minlength: 5, maxlength: 25, required: true },
+  role: { type: String, enum: ["tutor", "student"], default: "user" },
 });
 
 UserSchema.pre("save", async function (next) {
-    const salt = await bcrypt.genSalt(10);
-    console.log({salt});
-    const hashedPassword = await bcrypt.hash(this.password, salt);
-    this.password = hashedPassword;
-    console.log({hashedPassword});
-    next();
+  const salt = await bcrypt.genSalt(10);
+  console.log({ salt });
+  const hashedPassword = await bcrypt.hash(this.password, salt);
+  this.password = hashedPassword;
+  console.log({ hashedPassword });
+  next();
 });
 
 UserSchema.methods.isPasswordValid = async function (password) {
-    const compare = await bcrypt.compare(password, this.password);
-    return compare;
+  const compare = await bcrypt.compare(password, this.password);
+  return compare;
 };
 
 export const User = mongoose.model("User", UserSchema);
